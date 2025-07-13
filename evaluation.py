9ce2074dc472f0c020cd2f16b10bb976e3b1590e
Saeed Abokhatwa <64766080+Abokhatwa@users.noreply.github.com>
2025-07-13 13:18:22 +0100

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -63,6 +63,7 @@ def official_evaluate(tmp, path, tag, args):
     '''
         Adapted from the official evaluation code
     '''
+    path = '/kaggle/working'
     truth_dir = os.path.join(path, 'ref')
 
     if not os.path.exists(truth_dir):
